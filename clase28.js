721ba62f10f91c840192971f62447cf37e949dab
josecalvano1546 <38114264+josecalvano1546@users.noreply.github.com>
2019-09-28 22:46:47 -0300

    clase_js

--- clase28.js
+++ clase28.js
@@ -9,9 +9,19 @@ Esta función se invoca, después, dentro de la función externa para completar 
 //De esta manera se obtiene datos de la api SWAPI.
 const API_URL='https://swapi.co/api/'
 const PEOPLE_URL='people/:id'
-const lukurl=`${API_URL}${PEOPLE_URL.replace(':id', 1)}`
+
 const opts={ crossDomain: true }
 const onRepleace =(luke) => console.log(`Hola soy ${luke.name}`)
 
-$.get(lukurl, opts, onRepleace)
+function obtenerPersonaje(id){
+    const lukurl=`${API_URL}${PEOPLE_URL.replace(':id', id)}`
+    $.get(lukurl, opts, onRepleace)
+}
+/* De esta manera podemos ver como funciona el asincronismo en JS, no podemos saber con 
+anticipacion en que orden obtendremos los personajes*/
+obtenerPersonaje(1)
+obtenerPersonaje(2)
+obtenerPersonaje(3)
+obtenerPersonaje(4)
+
 
