9e796ad5da79778889244ecf694a4942289bd8b2
josecalvano1546 <38114264+josecalvano1546@users.noreply.github.com>
2019-09-28 22:27:17 -0300

    clase_js

---
+++ clase28.js
@@ -0,0 +1,17 @@
+//                      Callbacks
+/*
+En esta clase aprenderemos que son los callbacks y usaremos una librería externa que se llama jQuery.
+
+Un callback es una función que se pasa a otra función como un argumento. 
+Esta función se invoca, después, dentro de la función externa para completar alguna acción.
+*/
+
+//De esta manera se obtiene datos de la api SWAPI.
+const API_URL='https://swapi.co/api/'
+const PEOPLE_URL='people/:id'
+const lukurl=`${API_URL}${PEOPLE_URL.replace(':id', 1)}`
+const opts={ crossDomain: true }
+const onRepleace =(luke) => console.log(`Hola soy ${luke.name}`)
+
+$.get(lukurl, opts, onRepleace)
+
